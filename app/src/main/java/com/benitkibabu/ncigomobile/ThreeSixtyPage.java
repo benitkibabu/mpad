880d6365d821d80301e5c8af8fcf3e9bdedab611
benitkibabu <benitkibabu@gmail.com>
2015-12-01 20:45:08 +0000

    version 1.0.1.5

--- app/src/main/java/com/benitkibabu/ncigomobile/ThreeSixtyPage.java
+++ app/src/main/java/com/benitkibabu/ncigomobile/ThreeSixtyPage.java
@@ -1,18 +1,32 @@
 package com.benitkibabu.ncigomobile;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.benitkibabu.adapters.CustomPagerAdapter;
 import com.benitkibabu.fragments.NciThreeSixtyFragment;
+import com.benitkibabu.helper.DbHelper;
+import com.benitkibabu.models.Timetable;
 import com.benitkibabu.sections_360.AppointmentFragment;
 import com.benitkibabu.sections_360.ServiceFragment;
 
@@ -21,7 +35,7 @@ import java.util.List;
 
 public class ThreeSixtyPage extends AppCompatActivity {
 
-
+    public static DbHelper db;
     private CustomPagerAdapter mSectionsPagerAdapter;
 
     private List<Fragment> fragmentList;
@@ -36,6 +50,8 @@ public class ThreeSixtyPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        db = new DbHelper(this);
+
         fragmentList = new ArrayList<>();
         addFragments();
         mSectionsPagerAdapter = new CustomPagerAdapter(getSupportFragmentManager(), fragmentList,
@@ -47,6 +63,14 @@ public class ThreeSixtyPage extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.new_service);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showEditDialog();
+            }
+        });
     }
 
     void addFragments(){
@@ -82,4 +106,56 @@ public class ThreeSixtyPage extends AppCompatActivity {
         }
     }
 
+    void showEditDialog(){
+        DialogFragment newFragment = ShowInsertDialog.newInstance("new_service");
+        newFragment.show(getSupportFragmentManager(), "new_service");
+    }
+
+    public static class ShowInsertDialog extends AppCompatDialogFragment {
+        private static final String ITEM_NAME = "name";
+
+        public static ShowInsertDialog newInstance(String name){
+            ShowInsertDialog fragment = new ShowInsertDialog();
+            Bundle args = new Bundle();
+            args.putString(ITEM_NAME, name);
+            fragment.setArguments(args);
+
+            return fragment;
+        }
+
+        public ShowInsertDialog(){}
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            final View view  = inflater.inflate(R.layout.new_service_layout, null);
+
+            final Spinner ns_service = (Spinner) view.findViewById(R.id.ns_service);
+            final Spinner ns_topic = (Spinner) view.findViewById(R.id.ns_topic);
+            final TextView ns_subject = (TextView) view.findViewById(R.id.ns_subject);
+            final TextView ns_body = (TextView) view.findViewById(R.id.ns_bodyText);
+
+            builder.setIcon(R.drawable.ic_information_black_48dp);
+            builder.setTitle("New Service");
+            builder.setCancelable(false);
+            builder.setView(view).setPositiveButton(R.string.add_string,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            Snackbar.make(view, "Added", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            ShowInsertDialog.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
+    }
 }
